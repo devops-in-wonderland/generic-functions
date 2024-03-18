d00c0d59576c4524343f2b0db9dd86d73fe23383
Erdinç Akgün <erdinc.akgun@icloud.com>
2024-03-19 00:09:55 +0300

    added aws_cost_report_yesterday

--- slack/functions.py
+++ slack/functions.py
@@ -1,5 +1,14 @@
+from datetime import datetime, timedelta
 import requests
 import json
+import boto3
+import dotenv
+import os
+import pandas as pd
+
+
+dotenv.load_dotenv(override=True)
+
 
 def send_message(token: str, channel_id: str, text: str) -> requests.Response:
     """This function is used send to message to Slack.
@@ -31,3 +40,70 @@ def send_message(token: str, channel_id: str, text: str) -> requests.Response:
 
     return response
 
+
+def aws_cost_report_yesterday(token: str, channel_id: str, fault_contact: str):
+    client = boto3.client('ce')
+
+    current_date = datetime.now()
+    yesterday_date = current_date - timedelta(days=1)
+
+    date_format = "%Y-%m-%d"
+    today_text = current_date.strftime(date_format)
+    yesterday_text = yesterday_date.strftime(date_format)
+
+    response: dict = client.get_cost_and_usage(
+        TimePeriod={
+            'Start': yesterday_text,
+            'End': today_text
+        },
+        Granularity='DAILY',
+        Metrics=["UnblendedCost"],
+        GroupBy=[
+            {
+                'Type': 'DIMENSION',
+                'Key': 'SERVICE'
+            },
+        ],
+    )
+
+    if 'NextPageToken' in response.keys():
+        send_message(token, channel_id,
+                     f"<@{fault_contact} aws_cost_report response contain NextPageToken!>")
+        # TODO should add logging
+        exit(1)
+
+    try:
+        if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
+            send_message(
+                token, channel_id, f"<@{fault_contact} aws_cost_report response is not 200!>")
+    except:
+        send_message(token, channel_id,
+                     f"<@{fault_contact} aws_cost_report could not get response properly!>")
+
+    results = response['ResultsByTime'][0]
+
+    with open("jotter.response.json", "w", encoding="utf-8") as f:
+        json.dump(results, f)
+
+    total_cost = 0
+    for group in results['Groups']:
+        total_cost += float(group['Metrics']['UnblendedCost']['Amount'])
+
+    message = f"*AWS Daily Cost Report*\n"
+    message += f"{results['TimePeriod']['Start']} \
+        Total cost: {total_cost:.2f} USD\n"
+
+    data = [(item["Keys"][0],
+             float(item["Metrics"]["UnblendedCost"]["Amount"]),
+             item["Metrics"]["UnblendedCost"]["Unit"]) for item in results['Groups']]
+
+    df = pd.DataFrame(data)
+    df.columns = ["Service", "Amount", "Unit"]
+    df.sort_values("Amount", inplace=True, ascending=False)
+    df['Amount'] = df['Amount'].apply("{:.2f}".format)
+
+    message += f"```{df.to_string(index=False)}```\n"
+
+    message += f"_This report generated from AWS Cost Explorer's estimated data._"
+
+    send_message(token, channel_id, message)
